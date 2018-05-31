907cd38e296f61b71562b30b7617a0b6de7c5315
sahithivodnala <15wh1a1213@bvrithyderabad.edu.in>
2018-05-31 16:21:43 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Angular Router</h1>
-  <nav>
-    <a routerLink="/home" routerLinkActive="active">Home</a>
-    <a routerLink="/login" routerLinkActive="active">Login</a>
-  </nav>
-  <router-outlet></router-outlet>
-`,
-  
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
-
+import {HttpClient} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+import {ApplicableRefactorInfo} from 'typescript';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login',      component: LoginComponent },
+  { path: 'register',      component: RegisterComponent },
   ];
 
-@NgModule({ 
-  declarations: [
+@NgModule({
+    declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       {enableTracing: true}
